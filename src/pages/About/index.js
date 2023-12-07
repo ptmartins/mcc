1ddb670bf24f8d9378463c50fa76c1e31e0fccc9
ptmartins <pedro@pedromartins.com.pt>
2023-12-07 19:09:31 +0000

    Basic routing

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,11 @@
-import { Card } from '../../components';
+import { PageLayout } from '../../components';
 
 export default function About() {
     return(
-        <h1>About</h1>    
+        <PageLayout title="About">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde iste saepe perferendis fugit cum nihil veritatis dignissimos exercitationem cupiditate veniam recusandae accusantium hic ratione facilis quis aliquam distinctio, fugiat alias?
+ 
+        </PageLayout>
+  
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,8 +2,11 @@ import { PageLayout } from '../../components';
 
 export default function Home() {
     return(
-        <PageLayout title="FFT">
-            <h1>Home odjoejwdoj</h1>  
+        <PageLayout title="Home">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde iste saepe perferendis fugit cum nihil veritatis dignissimos exercitationem cupiditate veniam recusandae accusantium hic ratione facilis quis aliquam distinctio, fugiat alias?
+            Iusto dolorum molestias accusantium architecto eaque praesentium et distinctio nisi iste. Voluptates in laudantium quae magnam iste optio explicabo quaerat aliquid hic ea, tenetur, quam debitis, asperiores error quas modi.
+            Minima quibusdam assumenda voluptates tempora maiores dolore repudiandae quos odit error consequuntur sed voluptatem aspernatur eligendi, incidunt totam, amet impedit delectus! Sequi provident aut inventore, nam fugit commodi vero voluptatibus!
+            Officia libero optio ea quod officiis corporis qui inventore molestiae, consectetur fugit aliquam. Minus sit quod quaerat error? Dignissimos delectus optio fugiat beatae id tempore commodi quos vel vitae dolores!  
         </PageLayout>
   
     )
