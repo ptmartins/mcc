d19fe57291d29b2f618425be6a814996e4f8ec2a
ptmartins <pedro@pedromartins.com.pt>
2023-12-08 17:01:27 +0000

    Add diagnostics data.

--- start-json-server.js
+++ start-json-server.js
@@ -10,13 +10,15 @@ const data1 = require('./data/info.json');
 const data2 = require('./data/license.json');
 const data3 = require('./data/general.json');
 const data4 = require('./data/certificates.json');
+const data5 = require('./data/diagnostics.json');
 
 const combinedData = {
   info: data1.info,
   license: data2.licences,
   plugins: data2.plugins,
   general: data3,
-  certificates: data4.certificates
+  certificates: data4.certificates,
+  diagnostics: data5.dumpfiles
 };
 
 const router = jsonServer.router(combinedData);
