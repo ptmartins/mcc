f485c8324c8dbfe6e52888acca72d71175664386
ptmartins <pedro@pedromartins.com.pt>
2023-12-08 08:57:59 +0000

    Update date time every second, to show live clock.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
+
+    const [dateTime, setDateTime] = useState(new Date().toLocaleString());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setDateTime(new Date().toLocaleString());
+        }, 1000);
+    
+        return () => clearInterval(interval);
+      }, []);
+
     return(
         <footer className={styles.footer}>
-            <div className={styles.date}>{ new Date().toLocaleString() }</div>
+            <div className={styles.date}>{ dateTime }</div>
             <div className={styles.copyright}> Copyright &copy;2018 Imagen Ltd. </div>
         </footer>
     )
