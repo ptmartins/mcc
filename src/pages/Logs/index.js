c009436432f665906cd2e2a852d8d8a37cd58810
ptmartins <pedro@pedromartins.com.pt>
2023-12-09 18:55:38 +0000

    Start adding TabMenu to logs page

--- src/pages/Logs/index.js
+++ src/pages/Logs/index.js
@@ -1,10 +1,16 @@
-import { Card, PageLayout, Table} from '../../components';
+import { Card, PageLayout, TabMenu, Table} from '../../components';
 
 export default function Logs() {
+
+    const tabs =  [
+        { title: 'Menu 1', content: <p>This is the content of Tab 1.</p> },
+        { title: 'Menu 2', content: <p>This is the content of Tab 2.</p> },
+      ];
+
     return(
         <PageLayout title="Logs">
             <Card>
-                <Table />
+                <TabMenu items={ tabs }/>
             </Card>
         </PageLayout>
   
