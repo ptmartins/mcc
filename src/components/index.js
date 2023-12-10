62da0947713ff99bcf6c0e29286069087fd1e8b3
ptmartins <pedro@pedromartins.com.pt>
2023-12-10 12:09:57 +0000

    Add Accordion component.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+import Accordion from './Accordion/Accordion'
 import Button from './Button/Button';
 import Card from './Card/Card';
 import Footer from './Footer/Footer';
@@ -10,4 +11,4 @@ import PageLayout from './PageLayout/PageLayout';
 import Table from './Table/Table';
 import TabMenu from './TabMenu/TabMenu';
 
-export { Button, Card, Footer, Header, Input, KeyValue, LoginForm, Nav, PageLayout, Table, TabMenu };
+export { Accordion, Button, Card, Footer, Header, Input, KeyValue, LoginForm, Nav, PageLayout, Table, TabMenu };
