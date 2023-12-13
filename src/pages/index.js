739a752e4c03fc42ec1c803d4bc70db8d3a6654f
ptmartins <pedro@pedromartins.com.pt>
2023-12-13 13:42:24 +0000

    Add 'General' page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Applications from './Applications';
 import Certificates from './Certificates';
 import Databases from './Databases';
 import Diagnostics from './Diagnostics';
+import General from './General';
 import Home from './Home';
 import Logs from './Logs';
 import NotFound from './NotFound';
@@ -11,4 +12,4 @@ import RemoteTools from './RemoteTools';
 import ResourceGroups from './ResourceGroups';
 import Updates from './Updates';
 
-export { About, Account, Applications, Certificates, Databases, Diagnostics, Home, Logs, NotFound, RemoteTools, ResourceGroups, Updates };
+export { About, Account, Applications, Certificates, Databases, Diagnostics, General, Home, Logs, NotFound, RemoteTools, ResourceGroups, Updates };
